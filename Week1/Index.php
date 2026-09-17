8fc633fd4734184b81ca9098e8a408a4e6f26309
SadaamMohamud <zakariyepogba013@gmail.com>
2026-09-17 10:05:31 +0300

    Updated Echo and Print screenshots

--- Week1/Index.php
+++ Week1/Index.php
@@ -9,46 +9,10 @@
 
 <?php
 // PHP Tags
-echo "<h1>Welcome to PHP and SQL</h1>";
-
-// Echo
-echo "<h1>Welcome to PHP</h1>";
-
-// Print
-print "<h1>Welcome to PHP</h1>";
-
-// Print with single quotes
+//print with single quotes
 print '<h1>Welcome to PHP</h1>';
-
-// Print with double quotes
+//print with double quotes
 print "<h1>Welcome to PHP</h1>";
-
-// Echo multiple values using ,
-echo "<h1>Yahye", " Ali</h1>";
-
-// Print multiple values using .
-print "<h1>Yahye" . " Ali</h1>";
-
-// Single quotes using echo 
-echo '<h1>Welcome to PHP</h1>';
-
-//Double quotes using echo
-echo "<h1>Welcome to PHP</h1>";
-
-// Variable using echo and print
-$fullName = "Sadaam Mohamud Abdullahi";
-echo "<h1>My Name is $fullName</h1>";
-print "<h1>My Name is $fullName</h1>";
-
-// strlen() using with print and echo
-$my_str = 'welcome to php republic';
-echo strlen($my_str);
-print strlen($my_str);
-
-// str_word_count() using with echo and print 
-$my_str = 'welcome to php republic';
-echo str_word_count($my_str);
-print str_word_count($my_str);
 
 ?>
 
