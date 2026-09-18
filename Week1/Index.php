2a4bef1002f0501990d51664404ca2b43bf102bc
SadaamMohamud <zakariyepogba013@gmail.com>
2026-09-18 21:10:33 +0300

    Updated Week 1 Index.php

--- Week1/Index.php
+++ Week1/Index.php
@@ -3,16 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Introduction</title>
+    <title>Document</title>
 </head>
 <body>
 
 <?php
-// PHP Tags
+//Php Tags
+
+
+//Echo
+echo "<h1>Welcome to php and my sql</h1>";
+
+
+//print
+print "<h1>Welcome to php and my sql</h1>";
+
+
+//echo with single quotes
+echo '<h1>Welcome to PHP</h1>';
+
+//echo with double quotes
+echo "<h1>Welcome to PHP</h1>";
+
+
 //print with single quotes
 print '<h1>Welcome to PHP</h1>';
+
 //print with double quotes
 print "<h1>Welcome to PHP</h1>";
+
+
+//echo with parenthesis
+echo ("<h1>Welcome to php and my sql</h1>");
+
+
+//print with parenthesis
+print ("<h1>Welcome to php and my sql</h1>");
+
+
+//Echo with multiple values
+echo "<h1>PHP ", "MY SQL</h1>";
+
+
+//print with multiple values
+print "<h1>PHP " . "MY SQL</h1>";
+
+
+//strlen using echo and print
+$my_str = 'Welcome to php republic';
+echo strlen($my_str);
+print strlen($my_str);
+
+
+//str_word_count() using echo and print
+$my_str = 'welcome to php republic';
+echo str_word_count($my_str);
+print str_word_count($my_str);
+
+
+//Variable with using echo
+$Fullname = "Sadaam Mohamud Abdullahi";
+echo "<h1>My full Name is $Fullname</h1>";
+
+
+//Variable with using print
+$Fullname = "Sadaam Mohamud Abdullahi";
+print "<h1>My full Name is $Fullname</h1>";
 
 ?>
 
